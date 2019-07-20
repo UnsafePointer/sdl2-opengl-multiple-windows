100a9fdb717a56bf9af9192a5de56a5571356826
Renzo Crisostomo <renzo@crisostomo.me>
2019-07-20 11:27:36 +0200

    Add second window

--- src/main.cpp
+++ src/main.cpp
@@ -15,24 +15,36 @@ int main() {
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 5);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_DEBUG_FLAG);
-    SDL_Window *window = SDL_CreateWindow("main", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_OPENGL);
-    SDL_GLContext glContext = SDL_GL_CreateContext(window);
+
+    SDL_Window *mainWindow = SDL_CreateWindow("main", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_OPENGL);
+    SDL_GLContext glMainWindowContext = SDL_GL_CreateContext(mainWindow);
+
+    SDL_Window *secondaryWindow = SDL_CreateWindow("secondary", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_OPENGL);
+    SDL_GLContext glSeconraryWindowContext = SDL_GL_CreateContext(secondaryWindow);
+
     if (!gladLoadGLLoader((GLADloadproc) SDL_GL_GetProcAddress)) {
         cout << "Failed to initialize the OpenGL context." << endl;
         exit(1);
     }
     cout << "OpenGL " << GLVersion.major << "." << GLVersion.minor << endl;
-    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
     bool quit = false;
     while (!quit) {
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
+            if (event.window.event == SDL_WINDOWEVENT_CLOSE) {
                 quit = true;
             }
         }
+        SDL_GL_MakeCurrent(mainWindow, glMainWindowContext);
+        glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-        SDL_GL_SwapWindow(window);
+        SDL_GL_SwapWindow(mainWindow);
+
+        SDL_GL_MakeCurrent(secondaryWindow, glSeconraryWindowContext);
+        glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        SDL_GL_SwapWindow(secondaryWindow);
     }
     SDL_Quit();
     return 0;
